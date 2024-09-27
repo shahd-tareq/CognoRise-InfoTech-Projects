2b533beaa25fd199089219a74e8eea8efe955407
Shahd Tarek <shahddraz198@gmail.com>
2024-09-27 14:57:03 +0300

    Add files via upload

---
+++ cognorise2/script.js
@@ -0,0 +1,23 @@
+document.getElementById('bmiForm').addEventListener('submit', function(event) 
+    {
+    event.preventDefault();
+
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
+    
+    const bmi = weight / (height * height);
+    let category;
+
+    
+    if (bmi < 18.5) {
+        category = 'Underweight';
+    } else if (bmi < 24.9) {
+        category = 'Normal weight';
+    } else if (bmi < 29.9) {
+        category = 'Overweight';
+    } else {
+        category = 'Obesity';
+    }
+    document.getElementById('result').innerText = `Your BMI is ${bmi.toFixed(2)}: ${category}`;
+    
+});
